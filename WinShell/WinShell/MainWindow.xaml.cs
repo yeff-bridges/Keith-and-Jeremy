e48a5996a8804d053b8c0ea273598b672da35b7e
Keith Frechette <kfrechet@hotmail.com>
2018-08-10 00:08:04 -0400

    Initial Windows project creation with sample text input and output fields.

---
+++ WinShell/WinShell/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinShell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string CurrentWorkingDirectory { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            PresentCommandPrompt();
+        }
+
+        public void PresentCommandPrompt()
+        {
+            CurrentWorkingDirectory = Directory.GetCurrentDirectory();
+        }
+    }
+}
