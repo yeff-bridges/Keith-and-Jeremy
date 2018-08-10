001d8457aef7eee464aad214cd3a28439e70455b
Keith Frechette <kfrechet@hotmail.com>
2018-08-10 16:38:38 -0400

    UI updates:
    1. Adjust output windows.
    2. Add command history.
    3. Add simply class for processing commands.

---
+++ WinShell/WinShell/CommandProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinShell
+{
+    /// <summary>
+    /// Class for parsing and processing commands.
+    /// </summary>
+    public class CommandProcessor
+    {
+        /// <summary>
+        /// Parses and processes the specified command string.
+        /// </summary>
+        /// <param name="command">Command string to process.</param>
+        /// <param name="window">Window to use for command output.</param>
+        /// <returns>A value indicating whether we were able to successfully process the command.</returns>
+        public bool ProcessCommand(string command, MainWindow window)
+        {
+            window.WriteInfoText($"{window.CurrentWorkingDirectory} ==> {command}");
+            window.WriteOutputText($"Processing command: {command}");
+            Process.Start(command);
+            return true;
+        }
+    }
+}
--- WinShell/WinShell/MainWindow.xaml.cs
+++ WinShell/WinShell/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -23,16 +24,125 @@ namespace WinShell
     {
         public string CurrentWorkingDirectory { get; set; }
 
+        public List<string> CommandHistory { get; set; } = new List<string>();
+
+        public int IndexOfLastHistoryDisplayed { get; set; }
+
+        public CommandProcessor Processor { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
             PresentCommandPrompt();
+            Processor = new CommandProcessor();
         }
 
-        public void PresentCommandPrompt()
+        /// <summary>
+        /// Writes a string of informational (non-command output) to the command output area.
+        /// </summary>
+        /// <param name="outputText">String to output.</param>
+        public void WriteInfoText(string outputText)
+        {
+            stackOutputPanel.Children.Add(new TextBlock
+            {
+                Background = Brushes.Black,
+                Foreground = Brushes.LightGreen,
+                Text = outputText
+            });
+        }
+
+        /// <summary>
+        /// Writes a string of command output text to the command output area.
+        /// </summary>
+        /// <param name="outputText">String to output.</param>
+        public void WriteOutputText(string outputText)
+        {
+            stackOutputPanel.Children.Add(new TextBlock
+            {
+                Background = Brushes.Black,
+                Foreground = Brushes.White,
+                Text = outputText
+            });
+        }
+
+        private void PresentCommandPrompt()
         {
             CurrentWorkingDirectory = Directory.GetCurrentDirectory();
+            IndexOfLastHistoryDisplayed = -1;
+        }
+
+        private void btnOpenCurrentDir_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", CurrentWorkingDirectory);
+        }
+
+        private void ShowCommandFromHistory(int historyOffset)
+        {
+            // If we have any commands in our history...
+            if (CommandHistory.Count > 0)
+            {
+                int historyIndex;
+
+                // If we've not already displayed a history entry while entering the current command...
+                if (IndexOfLastHistoryDisplayed == -1)
+                {
+                    // Use the most recent history entry if we're advancing in a positive direction.
+                    if (historyOffset > 0)
+                    {
+                        historyIndex = CommandHistory.Count() - 1;
+                    }
+                    else
+                    {
+                        // Else use the oldest history entry.
+                        historyIndex = 0;
+                    }
+                }
+                else
+                {
+                    // Else advance to another history entry, and then wrap around if necessary.
+                    historyIndex = IndexOfLastHistoryDisplayed + historyOffset;
+                    if (historyIndex < 0)
+                    {
+                        historyIndex = CommandHistory.Count() - 1;
+                    }
+                    else if (historyIndex >= CommandHistory.Count)
+                    {
+                        historyIndex = 0;
+                    }
+                }
+
+                // Update the command input control to contain the history entry.
+                txtCommand.Text = CommandHistory[historyIndex];
+                txtCommand.CaretIndex = txtCommand.Text.Length;
+                IndexOfLastHistoryDisplayed = historyIndex;
+            }
+        }
+
+        private void txtCommand_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    var command = txtCommand.Text;
+
+                    CommandHistory.Add(command);
+                    //stackOutputPanel
+                    Processor.ProcessCommand(command, this);
+                    txtCommand.Text = string.Empty;
+                    e.Handled = true;
+                    break;
+
+                case Key.Up:
+                    ShowCommandFromHistory(1);
+                    e.Handled = true;
+                    break;
+
+                case Key.Down:
+                    ShowCommandFromHistory(-1);
+                    e.Handled = true;
+                    break;
+            }
         }
     }
 }
