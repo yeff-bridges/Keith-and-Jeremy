764dbfb0c576c4abe17463e2b303ccb913689fbc
Keith Frechette <kfrechet@hotmail.com>
2018-08-10 22:29:11 -0400

    UI and command processing updates.
    1: Add local output methods to command processor.
    2. Include sample delegate methods for dispatching commands.

--- WinShell/WinShell/CommandProcessor.cs
+++ WinShell/WinShell/CommandProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +13,55 @@ namespace WinShell
     /// </summary>
     public class CommandProcessor
     {
-        private string[] builtin_str =
+        private string[] _builtin_str =
         {
-            "cd"
+            "cd",
+            "help",
+            "exit",
+            "pwd"
         };
 
-        private string[] builtin_sym =
+        private string[] _builtin_sym =
         {
             "|",
             "<",
             ">"
         };
 
+        delegate int CommandMethod(string[] args);
 
+        private CommandMethod[] _builtin_func;
+
+        private MainWindow _outputWindow;
+
+        public CommandProcessor()
+        {
+            _builtin_func = new CommandMethod[]
+            {
+                CommandCD,
+                CommandHelp,
+                CommandHelp,
+                CommandPrintWorkingDirectory,
+            };
+        }
+
+        int CommandCD(string[] args)
+        {
+            return 0;
+        }
+
+        int CommandHelp(string[] args)
+        {
+            return 0;
+        }
+
+        int CommandPrintWorkingDirectory(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            WriteOutputText($"Current directory: {currentDirectory}");
+
+            return 0;
+        }
 
         /// <summary>
         /// Parses and processes the specified command string.
@@ -35,12 +72,29 @@ namespace WinShell
         public bool ProcessCommand(string command, MainWindow window)
         {
             bool builtin = false;
+            _outputWindow = window;
 
-            window.WriteInfoText($"{window.CurrentWorkingDirectory} ==> {command}");
-            window.WriteOutputText($"Processing command: {command}");
+            WriteInfoText($"{window.CurrentWorkingDirectory} ==> {command}");
+            WriteOutputText($"Processing command: {command}");
 
-            if(!builtin){
-                Launch(command, window);
+            int commandIndex = -1;
+            switch (command)
+            {
+                case "pwd":
+                    commandIndex = 3;
+                    break;
+            }
+
+            builtin = (commandIndex >= 0);
+
+            if (!builtin)
+            {
+                Launch(command);
+            }
+            else
+            {
+                var args = new string[] { "Arg0", "Arg1" };
+                _builtin_func[commandIndex](args);
             }
 
             return true;
@@ -49,20 +103,37 @@ namespace WinShell
         /// <summary>
         /// Creates a process specified by the command string.
         /// <param name="command">Command string to process.</param>
-        /// <param name="window">Window to use for command output.</param>
         /// <returns>A value indicating whether we were able to successfully launch a new process.</returns>
-        public bool Launch(string command, MainWindow window){
+        public bool Launch(string command){
             try
             {
                 Process.Start(command);
             }
             catch
             {
-                window.WriteInfoText($"Command failed. Could not find: {command}");
+                WriteInfoText($"Command failed. Could not find: {command}");
                 return false;
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Writes a string of informational (non-command output) to the command output area.
+        /// </summary>
+        /// <param name="outputText">String to output.</param>
+        private void WriteInfoText(string outputText)
+        {
+            _outputWindow.WriteInfoText(outputText);
+        }
+
+        /// <summary>
+        /// Writes a string of command output text to the command output area.
+        /// </summary>
+        /// <param name="outputText">String to output.</param>
+        private void WriteOutputText(string outputText)
+        {
+            _outputWindow.WriteOutputText(outputText);
         }
     }
 }
