1cc45d96532f31b8f1e4d0431854a9a6447ff2e2
Jeremy Frechette <jeremyAFrechette@gmail.com>
2018-08-10 18:38:21 -0700

    Slightly reformatted Command Processor

--- WinShell/WinShell/CommandProcessor.cs
+++ WinShell/WinShell/CommandProcessor.cs
@@ -12,6 +12,20 @@ namespace WinShell
     /// </summary>
     public class CommandProcessor
     {
+        private string[] builtin_str =
+        {
+            "cd"
+        };
+
+        private string[] builtin_sym =
+        {
+            "|",
+            "<",
+            ">"
+        };
+
+
+
         /// <summary>
         /// Parses and processes the specified command string.
         /// </summary>
@@ -20,16 +34,31 @@ namespace WinShell
         /// <returns>A value indicating whether we were able to successfully process the command.</returns>
         public bool ProcessCommand(string command, MainWindow window)
         {
+            bool builtin = false;
+
             window.WriteInfoText($"{window.CurrentWorkingDirectory} ==> {command}");
             window.WriteOutputText($"Processing command: {command}");
 
+            if(!builtin){
+                Launch(command, window);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Creates a process specified by the command string.
+        /// <param name="command">Command string to process.</param>
+        /// <param name="window">Window to use for command output.</param>
+        /// <returns>A value indicating whether we were able to successfully launch a new process.</returns>
+        public bool Launch(string command, MainWindow window){
             try
             {
                 Process.Start(command);
             }
             catch
             {
-                window.WriteInfoText("Command failed.");
+                window.WriteInfoText($"Command failed. Could not find: {command}");
                 return false;
             }
 
